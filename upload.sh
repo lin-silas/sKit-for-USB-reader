72be55274d7ba3999cddc27ef15da7e3b5167ef2
lin-silas <93635072+lin-silas@users.noreply.github.com>
2021-11-18 21:50:30 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,28 +1,15 @@
 #!/bin/sh
 
-env_set() {
-    TCE=/mnt/sda2/tce 
-    TCEO=$TCE/optional
-    ONB=$TCE/onboot.lst
-    sKitbase=$TCE/sKit
-    LOGDIR=$sKitbase/log
-    LOG=$LOGDIR/$fname.log
-  REPO_PCP="https://repo.picoreplayer.org/repo"
-  EXTENSIONS="\
-  curl" 
-  TIMEOUT=300
-}
-
 download_extensions() {
 
     echo -e "\tdownloading extensions (~3min master, ~5min mirror - for initial DL)"
-    sudo -u tc timeout 300 pcp-load -r "https://repo.picoreplayer.org/repo" -w "curl"
+    timeout 300 pcp-load -r "https://repo.picoreplayer.org/repo" -w "curl"
 }
 
 load_extensions() {
 
     echo -e "\tloading extensions (temporary)"
-    pcp-load -s -l -i "curl" >>$LOG 2>&1
+    pcp-load -s -l -i "curl"
 }
 
 upload_squeezlite() {
@@ -32,7 +19,6 @@ upload_squeezlite() {
 }
 
 ###main#######################################
-env_set
 download_extensions
 load_extensions
 upload_squeezlite
