56b2e9a1267da957e4a49a402e0526af04fdcb03
lin-silas <93635072+lin-silas@users.noreply.github.com>
2021-11-18 23:04:00 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -5,15 +5,8 @@
 
 useBusybox
 
-ext="curl"
-
-tce-load -i ca-certificates.tcz
-
-echo -e "\tdownloading extensions"
-tce-load -w $ext
-
 echo -e "\tloading extensions"
-tce-load -s -l -i $ext
+tce-load -i curl.tcz
 
 echo -e "\tuploading squeezelite-custom"
 sudo curl -T /mnt/sda2/tce/squeezelite-custom https://oshi.at
