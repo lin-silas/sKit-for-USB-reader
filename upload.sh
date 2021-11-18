f3dc3915a68773a9146d4ce3cbf780d313cfefb7
lin-silas <93635072+lin-silas@users.noreply.github.com>
2021-11-18 22:46:41 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -8,13 +8,13 @@ useBusybox
 REPO_PCP="https://repo.picoreplayer.org/repo"
 ext="curl"
 
-tce-load -i ca-certificates.tcz
+tce-load -i ca-certificates.tcz > /dev/null 2>&1
 
 echo -e "\tdownloading extensions"
-tce-load -w $ext
+tce-load -w $ext > /dev/null 2>&1
 
 echo -e "\tloading extensions"
-tce-load -s -l -i $ext
+tce-load -s -l -i $ext > /dev/null 2>&1
 
 echo -e "\tuploading squeezelite-custom"
 sudo curl -T /mnt/sda2/tce/squeezelite-custom https://oshi.at
